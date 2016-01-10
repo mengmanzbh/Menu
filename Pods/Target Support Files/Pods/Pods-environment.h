c2e1d602d0971b2d427b27d7f836b3acc4413f73
mengmanzbh <1934068305@qq.com>
2016-01-10 09:10:53 +0800

    11
    11

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KYGooeyMenu
+#define COCOAPODS_POD_AVAILABLE_KYGooeyMenu
+#define COCOAPODS_VERSION_MAJOR_KYGooeyMenu 1
+#define COCOAPODS_VERSION_MINOR_KYGooeyMenu 0
+#define COCOAPODS_VERSION_PATCH_KYGooeyMenu 5
+
--- test/ViewController.m
+++ test/ViewController.m
@@ -7,18 +7,33 @@
 //
 
 #import "ViewController.h"
+#import "KYGooeyMenu.h"
 
-@interface ViewController ()
+@interface ViewController ()<menuDidSelectedDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+      KYGooeyMenu *gooeyMenu;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    gooeyMenu = [[KYGooeyMenu alloc]initWithOrigin:CGPointMake(CGRectGetMidX(self.view.frame)-50, 500) andDiameter:100.0f andDelegate:self themeColor:[UIColor redColor]];
+    gooeyMenu.menuDelegate = self;
+    gooeyMenu.radius = 100/5;     //这里把小圆半径设为大圆的1/4
+    gooeyMenu.extraDistance = 20; //间距设为R+r+20。注：R+r是默认存在的。
+    gooeyMenu.MenuCount = 5;      //4个子菜单
+    gooeyMenu.menuImagesArray = [NSMutableArray arrayWithObjects:
+                                 [UIImage imageNamed:@"tabbarItem_discover highlighted"],
+                                 [UIImage imageNamed:@"tabbarItem_group highlighted"],
+                                 [UIImage imageNamed:@"tabbarItem_home highlighted"],
+                                 [UIImage imageNamed:@"tabbarItem_message highlighted"],
+                                 [UIImage imageNamed:@"tabbarItem_user_man_highlighted"], nil];
 }
-
+-(void)menuDidSelected:(int)index{
+    NSLog(@"选中第%d",index);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
