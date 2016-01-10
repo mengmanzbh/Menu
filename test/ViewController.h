94530f14bb22f7254c51c3d3ebc36c37eded7b75
mengmanzbh <1934068305@qq.com>
2016-01-10 09:01:05 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 谢霆锋 on 16/1/10.
+//  Copyright © 2016年 lll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 谢霆锋 on 16/1/10.
+//  Copyright © 2016年 lll. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
